4e78102c81ececfb5e230ee3a0a5f466bb797084
Raul Hahn <hahn.raul@gmail.com>
2017-03-15 10:36:58 +0200

    Added tango swift framework, and Rich notification framework

---
+++ TangoRichNotification.framework/Headers/TangoRichNotification.h
@@ -0,0 +1,19 @@
+//
+//  TangoRichNotification.h
+//  TangoRichNotification
+//
+//  Created by Raul Hahn on 12/16/16.
+//  Copyright © 2016 Tangotargetting. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TangoRichNotification.
+FOUNDATION_EXPORT double TangoRichNotificationVersionNumber;
+
+//! Project version string for TangoRichNotification.
+FOUNDATION_EXPORT const unsigned char TangoRichNotificationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TangoRichNotification/PublicHeader.h>
+
+
